8ce1fd2375fe60893b20ce7b5f6caa12fde405bb
김다인 <frambo1se@naver.com>
2024-01-31 15:58:06 +0900

    refactor: dto가 snake_case 형태 JSON 데이터 인식하도록 수정

--- backend/src/main/java/com/ssafy/puzzlepop/dm/domain/DmReadRequestDto.java
+++ backend/src/main/java/com/ssafy/puzzlepop/dm/domain/DmReadRequestDto.java
@@ -1,5 +1,7 @@
 package com.ssafy.puzzlepop.dm.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 @Getter
@@ -7,6 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class DmReadRequestDto {
 
     private Long userId;
--- backend/src/main/java/com/ssafy/puzzlepop/dm/domain/DmReadResponseDto.java
+++ backend/src/main/java/com/ssafy/puzzlepop/dm/domain/DmReadResponseDto.java
@@ -1,5 +1,7 @@
 package com.ssafy.puzzlepop.dm.domain;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
 import java.util.Date;
@@ -9,6 +11,7 @@ import java.util.Date;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class DmReadResponseDto {
 
     private Long id;
