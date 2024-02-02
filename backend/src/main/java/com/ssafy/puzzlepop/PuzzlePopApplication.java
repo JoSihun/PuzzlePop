891944e0ddb9c7f9b6017e7736010302016f6ba0
김한중 <hjhzkc@gmail.com>
2024-02-02 18:06:45 +0900

    feat: Add @ConfigurationPropertiesScan annotations in PuzzlePopApplication

--- backend/src/main/java/com/ssafy/puzzlepop/PuzzlePopApplication.java
+++ backend/src/main/java/com/ssafy/puzzlepop/PuzzlePopApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.puzzlepop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class PuzzlePopApplication {
 
 	public static void main(String[] args) {
