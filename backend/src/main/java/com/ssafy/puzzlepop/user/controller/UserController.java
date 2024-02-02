0ddc1afef31b6f4498668433ba4b5f4f47ef8790
김한중 <hjhzkc@gmail.com>
2024-02-02 18:06:06 +0900

    feat: Implement User Controllers

--- backend/src/main/java/com/ssafy/puzzlepop/user/controller/UserController.java
+++ backend/src/main/java/com/ssafy/puzzlepop/user/controller/UserController.java
@@ -16,20 +16,6 @@ public class UserController {
 
     private final UserService userService;
 
-
-
-//    @PostMapping("/user")
-//    public ResponseEntity<?> createUser(@RequestBody UserDto requestDto) {
-//        try {
-//            Long id = userService.createUser(requestDto);
-//            return ResponseEntity.status(HttpStatus.CREATED).body(id);
-//        } catch (UserNotFoundException e) {
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-//        } catch (Exception e) {
-//            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-//        }
-//    }
-
     @GetMapping("/user")
     public ResponseEntity<?> getUserByEmail(@RequestBody UserDto requestDto) {
         try {
