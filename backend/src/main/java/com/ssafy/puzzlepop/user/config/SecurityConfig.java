db600643c712f101336249ce18d5be2059058ec3
김한중 <hjhzkc@gmail.com>
2024-02-02 18:05:51 +0900

    feat: Implement User Configs

--- backend/src/main/java/com/ssafy/puzzlepop/user/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/puzzlepop/user/config/SecurityConfig.java
@@ -1,32 +1,82 @@
 package com.ssafy.puzzlepop.user.config;
 
+import com.ssafy.puzzlepop.user.exception.RestAuthenticationEntryPoint;
+import com.ssafy.puzzlepop.user.filter.TokenAuthenticationFilter;
 import com.ssafy.puzzlepop.user.handler.Oauth2AuthenticationFailureHandler;
 import com.ssafy.puzzlepop.user.handler.Oauth2AuthenticationSuccessHandler;
-import com.ssafy.puzzlepop.user.service.UserService;
+import com.ssafy.puzzlepop.user.service.CustomOAuth2UserService;
+import com.ssafy.puzzlepop.user.service.CustomUserDetailsService;
+import com.ssafy.puzzlepop.user.util.HttpCookieOAuth2AuthorizationRequestRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+// 변경 필요
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
 public class SecurityConfig {
 
-    private final UserService userService;
-//    private final Oauth2AuthenticationSuccessHandler oauth2AuthenticationSuccessHandler;
-    private final Oauth2AuthenticationFailureHandler oauth2AuthenticationFailureHandler;
+    private CustomUserDetailsService customUserDetailsService;
+    private CustomOAuth2UserService customOAuth2UserService;
+    private Oauth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private Oauth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
 
-    public SecurityConfig(UserService userService, Oauth2AuthenticationFailureHandler oauth2AuthenticationFailureHandler) {
-        this.userService = userService;
-        this.oauth2AuthenticationFailureHandler = oauth2AuthenticationFailureHandler;
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter();
     }
 
+    @Bean
+    public HttpCookieOAuth2AuthorizationRequestRepository cookieOAuth2AuthorizationRequestRepository() {
+        return new HttpCookieOAuth2AuthorizationRequestRepository();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+//    @Override
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors((cors) -> cors.disable())
                 .csrf((csrf) -> csrf.disable());
 
         http
@@ -36,19 +86,35 @@ public class SecurityConfig {
                 .httpBasic((basic) -> basic.disable());
 
         http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        http
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+                );
+
+        http
                 .oauth2Login((oauth2) -> oauth2
-                        .loginPage("/login")
-                                .userInfoEndpoint((userInfoEndpointConfig) ->
-                                        userInfoEndpointConfig.userService(userService))
-//                                .successHandler(oauth2AuthenticationSuccessHandler)
-//                                .failureHandler(oauth2AuthenticationFailureHandler)
-                        );
+                        .authorizationEndpoint((auth) -> auth
+                                .baseUri("/oauth2/authorization")
+                                .authorizationRequestRepository(cookieOAuth2AuthorizationRequestRepository()))
+                        .userInfoEndpoint((user) -> user
+                                .userService(customOAuth2UserService))
+                        .redirectionEndpoint(redirection -> redirection
+                                .baseUri("/login/oauth2/callback/*"))
+                        .successHandler(oAuth2AuthenticationSuccessHandler)
+                        .failureHandler(oAuth2AuthenticationFailureHandler)
+                );
 
-//        http
-//                .authorizeHttpRequests((auth) -> auth
-//                        .requestMatchers("/login/*").permitAll()
-//                        .anyRequest().authenticated());
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/auth/**", "/oauth2/**").permitAll()
+                        .anyRequest().authenticated());
 
+
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
