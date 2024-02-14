053c93feeedcdb2e09d177d633009e8738e99a5d
조시훈 <kepler452k@naver.com>
2024-02-15 03:36:12 +0900

    test: Unit Test GameInfoControllerTest

--- backend/src/test/java/com/ssafy/puzzlepop/gameinfo/controller/GameInfoControllerTest.java
+++ backend/src/test/java/com/ssafy/puzzlepop/gameinfo/controller/GameInfoControllerTest.java
@@ -1,10 +1,13 @@
 package com.ssafy.puzzlepop.gameinfo.controller;
 
+import com.ssafy.puzzlepop.gameinfo.domain.GameInfo;
 import com.ssafy.puzzlepop.gameinfo.domain.GameInfoDto;
 import com.ssafy.puzzlepop.gameinfo.service.GameInfoService;
+import com.ssafy.puzzlepop.gameinfo.service.GameInfoServiceImpl;
 import com.ssafy.puzzlepop.user.filter.TokenAuthenticationProcessingFilter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
@@ -16,6 +19,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,10 +27,17 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -39,7 +50,7 @@ class GameInfoControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    private GameInfoService gameInfoService;
+    private GameInfoServiceImpl gameInfoService;
 
     @MockBean
     TokenAuthenticationProcessingFilter tokenAuthenticationProcessingFilter;
@@ -53,17 +64,50 @@ class GameInfoControllerTest {
     }
 
     @Test
+    @DisplayName("게임 정보 조회")
     @WithMockUser(username = "admin", roles = {"ADMIN"})
     void readGameInfo() throws Exception {
         // given
-        GameInfoDto responseDto = gameInfoService.readGameInfo(1L);
+        GameInfo gameInfo = GameInfo.builder()
+                .id(1L)
+                .type("battle")
+                .isCleared(false)
+                .curPlayerCount(0)
+                .maxPlayerCount(4)
+                .totalPieceCount(100)
+                .limitTime(null)
+                .passedTime(null)
+                .startedTime(null)
+                .finishedTime(null)
+                .build();
+        GameInfoDto responseDto = new GameInfoDto(gameInfo);
+        given(gameInfoService.readGameInfo(anyLong())).willReturn(responseDto);
 
         // when and then
         mockMvc.perform(get("/gameinfo")
                         .contentType(MediaType.APPLICATION_JSON)
                         .param("gameId", "1"))
+                .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("gameinfo-read", responseFields()));
+                .andDo(document("gameinfo-read",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        queryParameters(
+                                parameterWithName("gameId").description("게임 ID")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").type(JsonFieldType.NUMBER).description("게임 ID"),
+                                fieldWithPath("type").type(JsonFieldType.STRING).description("게임 종류"),
+                                fieldWithPath("isCleared").type(JsonFieldType.NULL).description("클리어 여부"),
+                                fieldWithPath("curPlayerCount").type(JsonFieldType.NUMBER).description("현재 플레이어 수"),
+                                fieldWithPath("maxPlayerCount").type(JsonFieldType.NUMBER).description("최대 플레이어 수"),
+                                fieldWithPath("totalPieceCount").type(JsonFieldType.NUMBER).description("총 조각 수"),
+                                fieldWithPath("limitTime").type(JsonFieldType.NULL).description("제한 시간"),
+                                fieldWithPath("passedTime").type(JsonFieldType.NULL).description("경과 시간"),
+                                fieldWithPath("startedTime").type(JsonFieldType.NULL).description("시작 시간"),
+                                fieldWithPath("finishedTime").type(JsonFieldType.NULL).description("종료 시간")
+                        )
+                ));
     }
 
     @Test
